f364b254cd61f02f78b428de5aff02a9978116af
Alexander Trauzzi <atrauzzi@gmail.com>
2014-02-02 17:55:34 -0600

    Update Image.php
    Clean up more indentation.

--- src/TippingCanoe/Imager/Model/Image.php
+++ src/TippingCanoe/Imager/Model/Image.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model {
 
-    protected $table = 'imager_image';
+	protected $table = 'imager_image';
 
     	protected $fillable = [
 		'imageable_id', 
