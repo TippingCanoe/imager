cdf539e96299230877867845801c467fdf03e359
Tyler Payne <tyler.payne@wmich.edu>
2014-02-21 15:45:15 -0500

    added a default amazon S3 configuration to the config.php file.

--- src/config/config.php
+++ src/config/config.php
@@ -11,8 +11,14 @@
 			// Public, client-accessible prefix pointing to wherever the root is hosted, including scheme.
 			'public_prefix' => sprintf('%s/imager', Request::getSchemeAndHttpHost()),
 
-		],
-
-	],
+        ],
+        /* Use this configuration for the Amazon S3 Driver
+        'TippingCanoe\Imager\Storage\S3' => [
+            'aws_key' => 'YOUR_KEY_HERE',
+            'aws_secret' => 'YOUR_SECRET_HERE',
+            'aws_bucket' => 'imager-bucket',
+        ],
+        */
+    ],
 
 ];
