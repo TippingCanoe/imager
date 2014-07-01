f0fc2064bbbe92acd4343fbb4af52ec0971be569
Colin Viebrock <c.viebrock@tippingcanoe.com>
2014-07-01 13:46:54 -0500

    more Intervention 2.0 changes

--- src/TippingCanoe/Imager/Processing/FixRotation.php
+++ src/TippingCanoe/Imager/Processing/FixRotation.php
@@ -9,27 +9,9 @@ class FixRotation implements Filter {
 
 	public function process(File $file, Image $image, array $config = null) {
 
-		// Only jpeg images support EXIF.
-		if($image->mime_type != 'image/jpeg')
-			return;
-
 		$imageData = Intervention::make($file->getRealPath());
 
-		switch($imageData->exif('Orientation')) {
-
-			case 8:
-				$imageData->rotate(90);
-			break;
-
-			case 3:
-				$imageData->rotate(180);
-			break;
-
-			case 6:
-				$imageData->rotate(-90);
-			break;
-
-		}
+		$imageData->orientate();
 
 		$imageData->save(null, 100);
 
